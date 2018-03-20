51e55bcb1bb0849ffc2c80b6ef0b95c0700a4be4
Paul Rowe <paul.louis.rowe@gmailcom>
2018-03-20 00:48:57 -0700

    updating

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -32,43 +32,9 @@ $(document).ready(function() {
 
     // Add a note
     $("#myBtn").click(function() {
-
+        console.log($(this).data('id'));
+          $("#articleId").val($(this).data('id'));
         $("#myModal").modal();
     });
 });
 
-function handleArticleNotes() {
-    // This function handles opending the notes modal and displaying our notes
-    // We grab the id of the article to get notes for from the panel element the delete button sits inside
-    var currentArticle = $(this).parents(".panel").data();
-    // Grab any notes with this headline/article id
-    $.get("/api/notes/" + currentArticle._id).then(function(data) {
-        // Constructing our initial HTML to add to the notes modal
-        var modalText = [
-            "<div class='container-fluid text-center'>",
-            "<h4>Notes For Article: ",
-            currentArticle._id,
-            "</h4>",
-            "<hr />",
-            "<ul class='list-group note-container'>",
-            "</ul>",
-            "<textarea placeholder='New Note' rows='4' cols='60'></textarea>",
-            "<button class='btn btn-success save'>Save Note</button>",
-            "</div>"
-        ].join("");
-        // Adding the formatted HTML to the note modal
-        bootbox.dialog({
-            message: modalText,
-            closeButton: true
-        });
-        var noteData = {
-            _id: currentArticle._id,
-            notes: data || []
-        };
-        // Adding some information about the article and article notes to the save button for easy access
-        // When trying to add a new note
-        $(".btn.save").data("article", noteData);
-        // renderNotesList will populate the actual note HTML inside of the modal we just created/opened
-        renderNotesList(noteData);
-    });
-}
--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get("/", function(req, res) {
 
 app.get("/saved", function(req, res) {
     db.Article.find({ isSaved: true })
+        .populate("notes")
         .then(function(dbArticles) {
             res.render("saved", { articles: dbArticles });
         })
@@ -74,44 +75,19 @@ app.put("/article/:id/unsave", function(req, res) {
         });
 })
 
-// // Route for retrieving all Notes from the db
-// app.get("/notes", function (req, res) {
-//   // Find all Notes
-//   db.Note.find({})
-//     .then(function (dbNote) {
-//       // If all Notes are successfully found, send them back to the client
-//       res.json(dbNote);
-//     })
-//     .catch(function (err) {
-//       // If an error occurs, send the error back to the client
-//       res.json(err);
-//     });
-// });
 
-// Route for retrieving all Aricles from the db
-// app.get("/user", function(req, res) {
-//     // Find all Users
-//     db.User.find({})
-//         .then(function(dbUser) {
-//             // If all Users are successfully found, send them back to the client
-//             res.json(dbUser);
-//         })
-//         .catch(function(err) {
-//             // If an error occurs, send the error back to the client
-//             res.json(err);
-//         });
-// });
-
-// Route for saving a new Note to the db and associating it with a User
+// Route for saving a new Note to the db and associating it with an Article
 app.post("/submit", function(req, res) {
     // Create a new Note in the db
-    console.log('tada');
+    // var articleID = req.params;
+                // console.log(articleID);
     db.Note.create(req.body)
         .then(function(dbNote) {
-            // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
-            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            console.log(articleID);
+            // If a Note was created successfully, find the Article and push the new Note's _id to the Article's `notes` array
+            // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
             // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-            return db.User.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+            return db.Article.findByIdAndUpdate(articleID, { $push: { notes: dbNote._id } }, { new: true });
         })
         .then(function(dbUser) {
             // If the User was updated successfully, send it back to the client
@@ -123,21 +99,6 @@ app.post("/submit", function(req, res) {
         });
 });
 
-// // Route to get all User's and populate them with their notes
-// app.get("/populateduser", function (req, res) {
-//   // Find all users
-//   db.User.find({})
-//     // Specify that we want to populate the retrieved users with any associated notes
-//     .populate("notes")
-//     .then(function (dbUser) {
-//       // If able to successfully find and associate all Users and Notes, send them back to the client
-//       res.json(dbUser);
-//     })
-//     .catch(function (err) {
-//       // If an error occurs, send it back to the client
-//       res.json(err);
-//     });
-// });
 
 app.get("/scrape", function(req, res) {
 
